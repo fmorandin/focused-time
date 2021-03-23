830db39374f2b214ee71b25f393ed74c9f84fed2
Felipe Morandin <felipemorandin@gmail.com>
2021-03-23 16:41:52 -0300

    Extracting some part of the settings view to a separate file

---
+++ Focused Timer/Focused Timer/Settings/View/FormView.swift
@@ -0,0 +1,102 @@
+//
+//  Form.swift
+//  Focused Timer
+//
+//  Created by Felipe Morandin on 22/03/21.
+//
+
+import SwiftUI
+
+struct FormView: View {
+
+    @StateObject private var settingsViewModel: SettingsViewModel
+
+    init(viewModel: SettingsViewModel = SettingsViewModel(settingsModel: SettingsModel())) {
+        _settingsViewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        VStack {
+            // Focused time
+            HStack {
+                Text(Translation.settingsFocusDuration)
+                    .accessibility(identifier: Identifiers.lblFocusDuration)
+
+                Spacer()
+
+                TextField("", text: $settingsViewModel.focusedTime)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(width: 50)
+                    .multilineTextAlignment(.center)
+                    .accessibility(identifier: Identifiers.txtFocusedTime)
+            }
+            .padding(.top, 10)
+            .padding(.bottom, 10)
+
+            Divider()
+
+            // Resting time
+            HStack {
+                Text(Translation.settingsRestDuration)
+                    .accessibility(identifier: Identifiers.lblRestDuration)
+
+                Spacer()
+
+                TextField("", text: $settingsViewModel.restTime)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(width: 50)
+                    .multilineTextAlignment(.center)
+                    .accessibility(identifier: Identifiers.txtRestTime)
+            }
+            .padding(.bottom, 10)
+            .padding(.top, 10)
+
+            Divider()
+
+            // Long Break
+            HStack {
+                Text(Translation.settingsLongBreak)
+                    .accessibility(identifier: Identifiers.lblLongBreak)
+
+                Spacer()
+
+                TextField("", text: $settingsViewModel.longBreak)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(width: 50)
+                    .multilineTextAlignment(.center)
+                    .accessibility(identifier: Identifiers.txtLongBreak)
+            }
+            .padding(.top, 10)
+            .padding(.bottom, 10)
+
+            Divider()
+
+            // Number of cycles
+            HStack {
+                Text(Translation.settingsCyclesTotal)
+                    .accessibility(identifier: Identifiers.lblCycleTotal)
+
+                Spacer()
+
+                TextField("", text: $settingsViewModel.cycleTotal)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(width: 50)
+                    .multilineTextAlignment(.center)
+                    .accessibility(identifier: Identifiers.txtCycleTotal)
+            }
+            .padding(.top, 10)
+            .padding(.bottom, 10)
+
+        }
+    }
+}
+
+struct Form_Previews: PreviewProvider {
+    static var previews: some View {
+        FormView()
+    }
+}
