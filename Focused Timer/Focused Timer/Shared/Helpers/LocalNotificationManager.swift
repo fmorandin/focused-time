79695b405f637e2ced7a628643daedee30dea5fb
Felipe Morandin <felipemorandin@gmail.com>
2026-02-19 23:08:19 +0000

    Testing Xcode Cloud trigger

--- Focused Timer/Focused Timer/Shared/Helpers/LocalNotificationManager.swift
+++ Focused Timer/Focused Timer/Shared/Helpers/LocalNotificationManager.swift
@@ -19,6 +19,7 @@ protocol UserNotificationCenterProtocol {
 }
 
 extension UNUserNotificationCenter: UserNotificationCenterProtocol {
+
     func setBadge(to value: Int) {
         setBadgeCount(value)
     }
